091dbd7e61aa39c123c3834245c2a48f6c81f37d
SanyamMadaan <124160436+SanyamMadaan@users.noreply.github.com>
2023-04-26 23:43:44 +0530

    Update Assignment9.c

--- Assignment9.c
+++ Assignment9.c
@@ -5,8 +5,35 @@
 //.       m = 1
 //.       n = 1
 
-int main()
-{
-  
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_SIZE 100
+
+int main() {
+  char name[MAX_SIZE];
+  int freq[26] = {0}; // Initialize all frequencies to 0
+
+  printf("Enter a name: ");
+  scanf("%s", name);
+
+  int len = strlen(name);
+
+  // Loop through each character in the name
+int i;
+  for (i = 0; i < len; i++) {
+    // Convert the character to its corresponding index in the frequency array
+    int index = name[i] - 'a';
+    freq[index]++;
+  }
+
+  // Print the frequency of each letter in the name
+
+  for ( i = 0; i < 26; i++) {
+    if (freq[i] > 0) {
+      printf("%c - %d\n", i+'a', freq[i]);
+    }
+  }
+
   return 0;
 }
