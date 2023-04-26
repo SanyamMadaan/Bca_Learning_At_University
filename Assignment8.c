2e98801945cbbcd30bee4f8e72c260045406b87f
SanyamMadaan <124160436+SanyamMadaan@users.noreply.github.com>
2023-04-26 23:40:04 +0530

    Update Assignment8.c

--- Assignment8.c
+++ Assignment8.c
@@ -2,7 +2,34 @@
 //input: Anitha
 //output: Aahint 
 
-int main()
-{
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+  char str[100];
+  int len;
+  char temp;
+
+  printf("Enter a string: ");
+  scanf("%s", str);
+
+  len = strlen(str);
+
+  // Bubble sort algorithm
+  int i;
+  for (i = 0; i < len - 1; i++) {
+    int j;
+	for ( j = 0; j < len - i - 1; j++) {
+      if (str[j] > str[j+1]) {
+        // Swapping characters using a temporary variable
+        temp = str[j];
+        str[j] = str[j+1];
+        str[j+1] = temp;
+      }
+    }
+  }
+
+  printf("Sorted string: %s\n", str);
+  
   return 0;
 }
